03d6b0479491b13fad1d3ec9c5c1ef93e35162c6
Glitch (hello-express) <none>
2023-01-21 14:54:37 +0000

    🌆💇 Checkpoint ./server.js:1433977/2

--- server.js
+++ server.js
@@ -45,6 +45,8 @@ const rgbToHex = (r, g, b) =>
     })
     .join("");
 
+
+
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
